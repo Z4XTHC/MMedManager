51306be19fa20c56b16c395439aa8f64876a91bf
Z4X <exeption.2015@gmail.com>
2025-02-02 16:26:58 -0300

    Se corrigio el error de guardar los datos del formulario cuando se lo envia al controller. --- --- Hay un problema dentro del SweetAlert2 que se cierra de manera automatica, por lo que se esta revisando donde ocurre dicho error.

--- target/classes/static/js/mMedManager.js
+++ target/classes/static/js/mMedManager.js
@@ -1,5 +1,4 @@
 // NUEVO PACIENTE POR MODAL
-// NUEVO PACIENTE POR MODAL
 $("#guardarPaciente").on("click", function () {
     let paciente = {
         nombre: $("#pNombre").val(),
@@ -17,10 +16,6 @@ $("#guardarPaciente").on("click", function () {
         data: JSON.stringify(paciente),
         contentType: "application/json",
         success: function (response) {
-            console.log(response);
-            $('#nuevoPacienteModal').modal('hide');
-            $('#tablaPacientes').DataTable().ajax.reload();
-
             Swal.fire({
                 title: "¡Éxito!",
                 text: "El paciente se guardó correctamente.",
@@ -28,16 +23,10 @@ $("#guardarPaciente").on("click", function () {
                 confirmButtonText: "Aceptar",
                 confirmButtonColor: "#e0a800",
                 background: "#495057",
-                color: "#fff",
-                timer: 2500,
-                timerProgressBar: true,
-                willClose: () => {
-                    location.reload();
-                }
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    location.reload();
-                }
+                color: "#fff"
+            }).then(() => {
+                $('#nuevoPacienteModal').modal('hide');
+                $('#tablaPacientes').DataTable().ajax.reload();
             });
         },
         error: function (error) {
