389afdca4b67f64997960b201376bd3fc7878acd
Z4X <exeption.2015@gmail.com>
2025-02-05 12:00:24 -0300

    refactor: Eliminación de edición de roles y configuración de datos por defecto
    - Se eliminó la funcionalidad de edición de roles existentes y la restricción para crear nuevos roles, ya que se utilizarán únicamente los roles creados por defecto.
    - Se creó el archivo import.sql para insertar datos por defecto en la base de datos.
    - Se agregaron los siguientes registros en import.sql para definir los roles predeterminados:
    
      `sql
      INSERT INTO mmedmanager_db.oles (
    ombre) VALUES ('Administrador');
      INSERT INTO mmedmanager_db.oles (
    ombre) VALUES ('Medico');
      INSERT INTO mmedmanager_db.oles (
    ombre) VALUES ('Recepcionista');

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `mmedmanager_db`.`roles` (`nombre`) VALUES ('Administrador');
+INSERT INTO `mmedmanager_db`.`roles` (`nombre`) VALUES ('Medico');
+INSERT INTO `mmedmanager_db`.`roles` (`nombre`) VALUES ('Recepcionista');
