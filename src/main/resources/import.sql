64f36b8003c4cdf5848d6bab80fe80274e94546d
Z4X <exeption.2015@gmail.com>
2025-02-06 10:12:31 -0300

    add: Creación de vistas y scripts para Usuarios y Médicos
    - Se añadieron las carpetas usuarios/ y medicos/ dentro de 	emplates/, organizando mejor las vistas del sistema.
    - Se crearon los archivos list.html en ambas carpetas, los cuales contienen las tablas para mostrar la información almacenada en la base de datos.
    - Se agregaron los archivos usuarios.js y medicos.js, encargados de la configuración de DataTables para gestionar la visualización de datos de manera dinámica.
    
     **Próxima actualización**:
    Se implementará la funcionalidad para crear, editar y eliminar Usuarios y Médicos mediante un **Modal**.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+/* ROLES CREADOS POR DEFAULT */
 INSERT INTO `mmedmanager_db`.`roles` (`nombre`) VALUES ('Administrador');
 INSERT INTO `mmedmanager_db`.`roles` (`nombre`) VALUES ('Medico');
 INSERT INTO `mmedmanager_db`.`roles` (`nombre`) VALUES ('Recepcionista');
---
+++ target/classes/static/js/medicos.js
@@ -0,0 +1,21 @@
+// TABLA USUARIOS
+$(document).ready(function () {
+    $('#tablaMedicos').DataTable({
+        lengthMenu: [5, 10, 15, 20],
+        language: {
+            "search": "Buscar: ",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "zeroRecords": "No hay registros",
+            "infoEmpty": "No hay registros",
+            "infoFiltered": "(Encontrados _MAX_ de registros)",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "previous": "Anterior",
+                "next": "Siguiente"
+            }
+        }
+    });
+
+});
